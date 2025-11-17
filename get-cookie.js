6792876655b2d145871179c2942b5f913c642204
Almer Andhika <almer@MIFX-IT-ALMER.local>
2025-11-17 11:13:44 +0700

    fix(puppeeter-instance): close browser after function execution finished

--- get-cookie.js
+++ get-cookie.js
@@ -1,14 +1,18 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
 export async function loginAndGetCookie() {
-    const username = process.env.UT_USERNAME;
+  const username = process.env.UT_USERNAME;
   const password = process.env.UT_PASSWORD;
-    if (!username || !password) {
-        throw new Error('Set UT_USERNAME and UT_PASSWORD environment variables before running.');
-    }
+  if (!username || !password) {
+    throw new Error(
+      'Set UT_USERNAME and UT_PASSWORD environment variables before running.'
+    );
+  }
 
-		const browser = await puppeteer.launch({ headless: true, args: [
-       '--no-sandbox',
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-session-crashed-bubble',
@@ -17,27 +21,34 @@ export async function loginAndGetCookie() {
       '--no-zygote',
       '--single-process',
       '--noerrdialogs',
-      '--disable-gpu'
-    ] });
-  	const page = await browser.newPage();
+      '--disable-gpu',
+    ],
+  });
+  const page = await browser.newPage();
 
-    const url = `https://pustaka.ut.ac.id/reader/index.php?modul=EKMA411603`;
-    await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('input#username');
-    await page.type('input#username', username, { delay: 50 });
-    await page.type('input#password', password, { delay: 50 });
-    // Captcha
-    await page.waitForSelector('span.captcha-text > strong');
-    const captchaText = await page.$eval('span.captcha-text > strong', el => el.innerText);
-    // Evaluate the captcha (simple math)
-    const captchaResult = eval(captchaText.replace(/[^-+*/0-9.()]/g, ''));
-    await page.type('input#ccaptcha', String(captchaResult));
-    await Promise.all([
-      page.click('button'),
-      page.waitForNavigation({ waitUntil: 'networkidle2' }),
-    ]);
+  const url = `https://pustaka.ut.ac.id/reader/index.php?modul=EKMA411603`;
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.waitForSelector('input#username');
+  await page.type('input#username', username, { delay: 50 });
+  await page.type('input#password', password, { delay: 50 });
+  // Captcha
+  await page.waitForSelector('span.captcha-text > strong');
+  const captchaText = await page.$eval(
+    'span.captcha-text > strong',
+    (el) => el.innerText
+  );
+  // Evaluate the captcha (simple math)
+  const captchaResult = eval(captchaText.replace(/[^-+*/0-9.()]/g, ''));
+  await page.type('input#ccaptcha', String(captchaResult));
+  await Promise.all([
+    page.click('button'),
+    page.waitForNavigation({ waitUntil: 'networkidle2' }),
+  ]);
 
-		const cookies = await page.cookies();
-		const cookieString = cookies.map(c => `${c.name}=${c.value}`).join('; ')
-		return cookieString
+  const cookies = await page.cookies();
+  const cookieString = cookies.map((c) => `${c.name}=${c.value}`).join('; ');
+
+  await browser.close();
+
+  return cookieString;
 }
